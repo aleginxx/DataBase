732f0550fbb2f776d5bf90ff3a6ffb9bcdb9de32
aleginxx <132457796+aleginxx@users.noreply.github.com>
2023-05-29 02:34:58 +0300

    Add files via upload

---
+++ scripts_for_connection/suspend_account.sql
@@ -0,0 +1,45 @@
+-- suspend accounts 
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS suspend_account //
+
+CREATE PROCEDURE suspend_account (
+	IN input_username VARCHAR(100)
+)
+BEGIN
+	DECLARE check_username INT;
+    
+    SELECT count(*) INTO check_username
+    FROM Central_Manager cm
+    WHERE cm.username_cm = input_username;
+    
+    IF check_username > 0 THEN SELECT 'You cannot suspend the Central Manager.' AS Message;
+    
+    ELSE
+		SELECT COUNT(*) INTO check_username
+        FROM Library_Manager lm
+        WHERE lm.username_libm = input_username;
+        
+		IF check_username > 0 THEN SELECT 'You cannot suspend the Library Manager.' AS Message;
+        
+        ELSE 
+			SELECT COUNT(*) INTO check_username
+            FROM School_User su
+            WHERE su.username_su = input_username;
+            
+			IF check_username > 0 THEN 
+				UPDATE School_User 
+                SET approved = 'suspended'
+                WHERE username_su = input_username ;
+			ELSE 
+				SELECT 'This account does not exist.' AS Message;
+			END IF;
+		END IF;
+	END IF;
+
+	END //
+    
+DELIMITER ;
+
+CALL suspend_account('godinternal');
