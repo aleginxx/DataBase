deb5b60bb46686e6dec3b26c9a05571e8106c565
aleginxx <132457796+aleginxx@users.noreply.github.com>
2023-05-28 15:33:22 +0300

    Delete Q3.1.7.sql

--- Q3.1.7.sql
+++
@@ -1,8 +0,0 @@
--- Q1.7
-SELECT a.first_name, a.last_name, COUNT(*) AS book_count
-FROM Author a
-JOIN Book_Has_Author bha ON a.author_id = bha.author_author_id
-JOIN Book b ON bha.book_isbn = b.isbn
-GROUP BY a.author_id, a.last_name, a.first_name
-ORDER BY book_count DESC
-LIMIT 1;
