5323c93c9f030c4b5507da81acc67aed2bce1906
aleginxx <132457796+aleginxx@users.noreply.github.com>
2023-05-28 15:32:54 +0300

    Delete Q3.1.4.sql

--- Q3.1.4.sql
+++
@@ -1,9 +0,0 @@
- select a.first_name, a.last_name
- from author a, Book_has_Author bha, book b
- where bha.Book_isbn=b.isbn
-	and a.author_id=bha.Author_author_id
-    and b.isbn not in (
-		select bd.isbn
-		from book_demand bd
-        where bd.category='B'
-	);
