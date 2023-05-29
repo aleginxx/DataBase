74150fcafd73e89ec1023753a55107f23c84f15f
aleginxx <132457796+aleginxx@users.noreply.github.com>
2023-05-29 23:08:35 +0300

    Add files via upload

--- Queries/Q3/Q3.3.2.sql
+++ Queries/Q3/Q3.3.2.sql
@@ -7,20 +7,24 @@ CREATE PROCEDURE get_list(
 )
 BEGIN
 
-    SET @t1 = 'SELECT DISTINCT b.title, CONCAT(su.first_name, " ", su.last_name) AS user_name, g.category
-        FROM book b, school_user su, book_demand bd, genre g
-        WHERE bd.isbn = b.isbn AND bd.username_su = su.username_su';
+SET @t1 = 'SELECT DISTINCT b.title, CONCAT(su.first_name, " ", su.last_name) AS user_name, GROUP_CONCAT(DISTINCT g.category SEPARATOR ", ") AS categories
+        FROM book b
+        JOIN book_demand bd ON bd.isbn = b.isbn
+        JOIN school_user su ON bd.username_su = su.username_su
+        JOIN Book_has_Genre bhg ON b.isbn = bhg.Book_isbn
+        JOIN genre g ON bhg.Genre_genre_id = g.genre_id';
 
-    IF search_name != '' THEN
-	SET @t1 = CONCAT(@t1, ' and CONCAT(su.first_name, '' '', su.last_name) LIKE ''%', search_name,'%''');
-	END IF;
-    
-    SET @t1 = CONCAT(@t1, ' GROUP BY user_name');
+IF search_name != '' THEN
+    SET @t1 = CONCAT(@t1, ' AND CONCAT(su.first_name, " ", su.last_name) LIKE "%', search_name, '%"');
+END IF;
 
-    -- Prepare and execute the dynamic SQL statement
-    PREPARE stmt3 FROM @t1;
-		EXECUTE stmt3;
-    DEALLOCATE PREPARE stmt3;
+SET @t1 = CONCAT(@t1, ' GROUP BY b.title, user_name');
+
+-- Prepare and execute the dynamic SQL statement
+PREPARE stmt3 FROM @t1;
+EXECUTE stmt3;
+DEALLOCATE PREPARE stmt3;
+
 END //
 
 DELIMITER ;
