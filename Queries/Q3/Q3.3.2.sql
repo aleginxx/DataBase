6d54cb54906a4ae8aee79610acc467717f976066
aleginxx <132457796+aleginxx@users.noreply.github.com>
2023-05-30 17:45:26 +0300

    Add files via upload

--- Queries/Q3/Q3.3.2.sql
+++ Queries/Q3/Q3.3.2.sql
@@ -3,22 +3,22 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS get_list //
 
 CREATE PROCEDURE get_list(
-    IN search_name VARCHAR(100)
+    IN search_username VARCHAR(100)
 )
 BEGIN
 
-SET @t1 = 'SELECT DISTINCT b.title, CONCAT(su.first_name, " ", su.last_name) AS user_name, GROUP_CONCAT(DISTINCT g.category SEPARATOR ", ") AS categories, bd.state 
+SET @t1 = 'SELECT DISTINCT b.title, CONCAT(su.first_name, " ", su.last_name) AS user_name, bd.state 
         FROM book b
         JOIN book_demand bd ON bd.isbn = b.isbn
         JOIN school_user su ON bd.username_su = su.username_su
         JOIN Book_has_Genre bhg ON b.isbn = bhg.Book_isbn
         JOIN genre g ON bhg.Genre_genre_id = g.genre_id';
 
-IF search_name != '' THEN
-    SET @t1 = CONCAT(@t1, ' AND CONCAT(su.first_name, " ", su.last_name) LIKE "%', search_name, '%"');
+IF search_username != '' THEN
+	SET @t1 = CONCAT(@t1, ' AND bd.username_su LIKE ''%', search_username, '%''');
 END IF;
 
-SET @t1 = CONCAT(@t1, ' AND bd.state IN ("active", "overdue")');
+SET @t1 = CONCAT(@t1, ' AND bd.state IN ("active", "overdue")  AND bd.category = ''B'' ');
 SET @t1 = CONCAT(@t1, ' GROUP BY b.title, user_name, bd.state');
 
 -- Prepare and execute the dynamic SQL statement
