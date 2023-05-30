cd0213ab8ad783c05e39dcc8cf4922419c10b346
Antwnia <132489812+Antwnia@users.noreply.github.com>
2023-05-30 18:35:33 +0300

    Update Q3.3.2.sql

--- Queries/Q3/Q3.3.2.sql
+++ Queries/Q3/Q3.3.2.sql
@@ -2,31 +2,22 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS get_list //
 
-CREATE PROCEDURE get_list(
-    IN search_username VARCHAR(100)
-)
+CREATE PROCEDURE get_list()
 BEGIN
-
-SET @t1 = 'SELECT DISTINCT b.title, CONCAT(su.first_name, " ", su.last_name) AS user_name, bd.state 
+    SET @t1 = CONCAT("SELECT DISTINCT b.title, CONCAT(su.first_name, \' \', su.last_name) AS user_name, bd.state 
         FROM book b
         JOIN book_demand bd ON bd.isbn = b.isbn
         JOIN school_user su ON bd.username_su = su.username_su
-        JOIN Book_has_Genre bhg ON b.isbn = bhg.Book_isbn
-        JOIN genre g ON bhg.Genre_genre_id = g.genre_id';
+        WHERE su.username_su = \'", "'.$connectedUsername.'", "\'
+        AND bd.state IN (\"active\", \"overdue\")
+        GROUP BY b.title, user_name, bd.state");
 
-IF search_username != '' THEN
-	SET @t1 = CONCAT(@t1, ' AND bd.username_su LIKE ''%', search_username, '%''');
-END IF;
+    -- Prepare and execute the dynamic SQL statement
+    PREPARE stmt3 FROM @t1;
+    EXECUTE stmt3;
+    DEALLOCATE PREPARE stmt3;
 
-SET @t1 = CONCAT(@t1, ' AND bd.state IN ("active", "overdue")  AND bd.category = ''B'' ');
-SET @t1 = CONCAT(@t1, ' GROUP BY b.title, user_name, bd.state');
-
--- Prepare and execute the dynamic SQL statement
-PREPARE stmt3 FROM @t1;
-EXECUTE stmt3;
-DEALLOCATE PREPARE stmt3;
-
-END //
+END
 
 DELIMITER ;
 
