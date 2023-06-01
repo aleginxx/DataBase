01bc47c90817e70f6f589a4f22e8ed30220597c7
aleginxx <132457796+aleginxx@users.noreply.github.com>
2023-06-01 21:09:54 +0300

    Update get_list_of_books.sql

--- scripts_for_connection/get_list_of_books.sql
+++ scripts_for_connection/get_list_of_books.sql
@@ -1,5 +1,7 @@
 -- get book list
 
-SELECT title, available_copies
-FROM Book
-ORDER BY title ASC ;
+SELECT b.title, CONCAT(a.first_name, " ", a.last_name) AS author_name
+FROM Book b
+JOIN Book_Has_Author bha ON b.isbn = bha.Book_isbn
+JOIN Author a ON bha.Author_author_id = a.author_id;
+ORDER BY b.title ASC; 
